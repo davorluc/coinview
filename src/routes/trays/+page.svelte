580664ca1c12d22c37a63594432740d05a69d2ce
davorluc <d.lucic4@hotmail.com>
2025-09-24 00:01:05 +0200

    added add tray button

--- src/routes/trays/+page.svelte
+++ src/routes/trays/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	let { data } = $props();
+
 	console.log(data);
+
+	let addingTray = $state(false);
+	let editingCoin = $state(false); // TODO: add editing functionality
+
+	function flipAdd() {
+		addingTray = !addingTray;
+	}
 </script>
 
+<button class="rounded bg-[#a31d1d] p-4 text-white" onclick={flipAdd}>Add tray+ </button>
 <div class="grid grid-cols-4 gap-6 p-4">
 	{#each data.trays as tray (tray.id)}
 		<div class="tray-coin rounded bg-white p-6 shadow">

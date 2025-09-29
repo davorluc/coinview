fa65dc8cf4395e07d7b2a4b61018ef989453007e
davorluc <d.lucic4@hotmail.com>
2025-09-29 22:50:07 +0200

    changed header styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,9 @@
 	let { children } = $props();
 </script>
 
-<header class="bg-cream flex items-center justify-between px-10 py-4">
+<header
+	class="fixed top-5 left-1/2 z-50 container flex w-full -translate-x-1/2 transform flex-row items-center justify-between rounded-xl bg-[#a31d1d] p-5 text-white shadow-xl"
+>
 	<h1 class="font-serif text-2xl">CoinView</h1>
 	<nav class="space-x-4">
 		<a href="/" class="hover:underline">Home</a>
@@ -14,7 +16,7 @@
 </header>
 <main>
 	<section class="px-10 py-20">
-		<div class="flex flex-col gap-10">
+		<div class="container flex flex-col gap-10">
 			{@render children()}
 		</div>
 	</section>
